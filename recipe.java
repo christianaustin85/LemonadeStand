218e78d6b7e0682f77000fbc6e1056b0a6b91afe
Christian A (Laptop) <christian.austin85@gmail.com>
2022-08-08 12:00:51 -0400

    merged with refactor. Adding recipe.java

--- recipe.java
+++ recipe.java
@@ -1,4 +1,10 @@
-
+/******************************************************************************
+ * Recipe.java 
+ * Christian Austin
+ *
+ * The class provides for different recipes to be made in the LemonadeStand
+ * program.  
+ ******************************************************************************/
 public class recipe {
 
 }
